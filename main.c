1a4486a02717d359f028e3b79e239facbe0fc37c
GulenayMer <61357481+GulenayMer@users.noreply.github.com>
2022-06-08 00:28:34 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -6,67 +6,81 @@
 /*   By: mgulenay <mgulenay@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 17:56:03 by mgulenay          #+#    #+#             */
-/*   Updated: 2022/06/05 18:47:31 by mgulenay         ###   ########.fr       */
+/*   Updated: 2022/06/07 23:24:40 by mgulenay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+#include <stdlib.h>
+#include <pthread.h>
 
-/* initiliazing the arguments */
-int	get_args(t_pro *process, int argc, char **argv)
+/*void *routine1()
 {
-	process->n_philos = ft_atoi(argv[1]);
-	process->time_to_die = ft_atoi(argv[2]);
-	process->time_to_eat = ft_atoi(argv[3]);
-	process->time_to_sleep = ft_atoi(argv[4]);
-	if (argc == 6)
-		process->n_meals = ft_atoi(argv[5]);
-	else
-		process->n_meals = -1;
+	printf("Test\n");
+	sleep(1);
+	printf("ending\n");
+	sleep(1);
 	return (0);
 }
 
-void	init_args(t_pro *process)
+void *routine2()
 {
-	
-		
-}
+	printf("ending\n");
+	sleep(1);
+	return(0);
+}*/
 
-/* pthread_create starts/initiliazes a new thread in the calling process */
-long int	philosophers(t_pro *process, pthread_t *tid_philosophers)
+/* pthread_create starts a new thread in the calling process */
+/*int	main(void)
 {
-	int			i;
-	int			*philosophers;
+	pthread_t	thread1;
+	pthread_t	thread2;
 
-	i = 0;
-	while (i < process->n_philos)
-	{
-		philosophers[i] = i;
-		if (pthread_create(&tid_philosophers[i], NULL, &routine, &philosophers[i]) != 0)
-			return (3);
-		i++;
-	}
-	i = 0;
-	while (i < process->n_philos)
-	{
-		if (pthread_join(tid_philosophers[i], NULL) != 0)
-			return (4);
-		i++;
-	}
-	return (0);
-}
-
-int	main(int argc, char **argv)
-{
-	t_pro		process;
-	pthread_t	*tid_philosophers;
-
-	if (argc < 5)
+	if (pthread_create(&thread1, NULL, routine1, NULL) != 0)
 		return (1);
-	tid_philosophers = malloc(sizeof(t_pro) * process.n_philos);
-	if (!tid_philosophers)
+	if (pthread_create(&thread2, NULL, routine1, NULL) != 0)
 		return (2);
-	get_args(&process, argc, argv);
-	philosophers(&process, tid_philosophers);
+	if (pthread_join(thread1, NULL) != 0)
+		return (3);
+	if (pthread_join(thread2, NULL) != 0)
+		return (4);
+	return (0);
+}*/
+
+/* create 10 threads, each taking a unique prime form the primes array
+ and print it on the screen. */
+
+void	*routine(void *arg)
+{
+	int	primes[10] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+	int	index;
+
+	index = *(int *)arg;
+	printf("%d\n", primes[index]);
+	return (0);
+}
+
+int	main()
+{
+	pthread_t	th[10];
+	int			i;
+	int			*a;
+
+	i = 0;
+	while (i < 10)
+	{		
+		a = malloc(sizeof(int));
+		*a = i;
+		if (pthread_create(&th[i], NULL, &routine, a) != 0)
+			write(1, "Failed to  create thread\n", 25);
+		i++;
+	}
+	i = 0;
+	while (i < 10)
+	{
+		if (pthread_join(th[i], NULL) != 0)
+			write(1, "Failed to join\n", 16);
+		i++;
+	}
 	return (0);
 }
